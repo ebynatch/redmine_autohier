b357a61bff1e37e80c686a1e07c1dc22b0440b6f
Dale Woolridge <dale.woolridge@gmail.com>
2012-10-16 01:04:21 -0400

    updated for 1.4 switch to hooks and javascript to swap in replacement breadcrumbs and add stylesheet to hide parent field in rename page. this way we don't have to maintain page copies and create conflicts. relocate some files out of app and into lib

--- init.rb
+++ init.rb
@@ -1,9 +1,22 @@
 require 'redmine'
+require 'autohier/hook'
 
 # Apply the wiki page patch
-require 'wiki_page_patch'
+#require 'wiki_page_patch'
 
-Redmine::Plugin.register :redmine_automatic_hierarchy do
+require 'dispatcher'
+Dispatcher.to_prepare :redmine_autohier do
+  unless WikiPage.included_modules.include? Autohier::WikiPageAh
+    WikiPage.send(:include, Autohier::WikiPageAh)
+  end
+
+  unless ApplicationHelper.included_modules.include? Autohier::ApplicationHelperAh
+    ApplicationHelper.send(:include, Autohier::ApplicationHelperAh)
+  end
+end
+
+
+Redmine::Plugin.register :redmine_autohier do
   name 'Redmine Automatic Hierarchy plugin'
   author 'Shinya Maeyama'
   description <<HERE
@@ -11,4 +24,5 @@ Replaces Redmine's wiki hierarchy features with a more transparent one.
 https://github.com/merikonjatta/redmine_autohier
 HERE
   version '0.0.4'
+  requires_redmine :version => ['1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8', '1.4.9']
 end
---
+++ lib/autohier/hook.rb
@@ -0,0 +1,6 @@
+module AutoHier
+  class AutoHierHook < Redmine::Hook::ViewListener
+    render_on :view_layouts_base_html_head, :partial => 'hooks/html_header_ah'
+    render_on :view_layouts_base_body_bottom, :partial => 'hooks/body_bottom_ah'
+  end
+end
