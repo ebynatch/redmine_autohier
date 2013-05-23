0a5ef00cb72fbfe91d898554a571fff884e96222
cferbar <claudio.sphinx@gmail.com>
2013-05-23 12:34:08 +0100

    changes to work in redmine 2.1.x

--- init.rb
+++ init.rb
@@ -4,14 +4,25 @@ require 'autohier/hook'
 # Apply the wiki page patch
 #require 'wiki_page_patch'
 
-require 'dispatcher'
-Dispatcher.to_prepare :redmine_autohier do
-  unless WikiPage.included_modules.include? Autohier::WikiPageAh
-    WikiPage.send(:include, Autohier::WikiPageAh)
-  end
+require 'dispatcher' unless Rails::VERSION::MAJOR >= 3
 
-  unless ApplicationHelper.included_modules.include? Autohier::ApplicationHelperAh
-    ApplicationHelper.send(:include, Autohier::ApplicationHelperAh)
+if Rails::VERSION::MAJOR >= 3
+  ActionDispatch::Callbacks.to_prepare do
+    unless WikiPage.included_modules.include? Autohier::WikiPageAh
+      WikiPage.send(:include, Autohier::WikiPageAh)
+    end
+    unless ApplicationHelper.included_modules.include? Autohier::ApplicationHelperAh
+      ApplicationHelper.send(:include, Autohier::ApplicationHelperAh)
+    end
+  end
+else
+  Dispatcher.to_prepare :redmine_autohier do
+    unless WikiPage.included_modules.include? Autohier::WikiPageAh
+      WikiPage.send(:include, Autohier::WikiPageAh)
+    end
+    unless ApplicationHelper.included_modules.include? Autohier::ApplicationHelperAh
+      ApplicationHelper.send(:include, Autohier::ApplicationHelperAh)
+    end
   end
 end
 
@@ -24,5 +35,4 @@ Replaces Redmine's wiki hierarchy features with a more transparent one.
 https://github.com/merikonjatta/redmine_autohier
 HERE
   version '0.0.4'
-  requires_redmine :version => ['1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.4.4', '1.4.5', '1.4.6', '1.4.7', '1.4.8', '1.4.9']
 end
--- lib/autohier/application_helper_ah.rb
+++ lib/autohier/application_helper_ah.rb
@@ -26,19 +26,19 @@ module Autohier
 	  pages[node].each do |page|
 	    content << "<li>"
 	    content << link_to(h(page.short_title), {:controller => 'wiki', :action => 'show', :project_id => page.project, :id => page.title},
-			       :title => (options[:timestamp] && page.respond_to?(:updated_on) ? l(:label_updated_time, distance_of_time_in_words(Time.now, page.updated_on)) : nil))
-	    content << "\n" + render_page_hierarchy(pages, page.id) if pages[page.id]
+			       :title => (options[:timestamp] && page.updated_on ? l(:label_updated_time, distance_of_time_in_words(Time.now, page.updated_on)) : nil))
+	    content << "\n" + render_page_hierarchy(pages, page.id, options) if pages[page.id]
 	    content << "</li>\n"
 	  end
 	  content << "</ul>\n"
 	end
-	content
+	content.html_safe
       end
 
       # Our version of Breadcrumbs
       def breadcrumb_with_autohier(*args)
 	elements = args.flatten
-	elements.any? ? content_tag('p', args.join('&gt;')+'&gt;'+@page.short_title, :class=>'breadcrumb') : nil
+	elements.any? ? content_tag('p', (args.join('&gt;')+'&gt;'+@page.short_title).html_safe, :class=>'breadcrumb') : nil
       end
     end
   end
